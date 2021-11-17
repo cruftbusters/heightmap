1d6c14226319da1c753fd6ea4f4d997316152e54
tyler <tyler@cruftbusters.com>
2021-11-17 14:41:23 -0600

    remove heightmap url check since the exclude query param does this

--- main.py
+++ main.py
@@ -24,12 +24,11 @@ def tick(baseURL):
         response = requests.get(f"{baseURL}/v1/layouts?excludeLayoutsWithHeightmap=true")
         if response.status_code == 200:
             for layout in response.json():
-                if layout["heightmapURL"] == "":
-                    heightmap = generate(sources, layout)
-                    with open(heightmap, 'rb') as f:
-                        requests.put(f"{baseURL}/v1/layouts/{layout['id']}/heightmap.jpg", f.read())
-                    os.remove(heightmap)
-                    os.remove(f"{heightmap}.aux.xml")
+                heightmap = generate(sources, layout)
+                with open(heightmap, 'rb') as f:
+                    requests.put(f"{baseURL}/v1/layouts/{layout['id']}/heightmap.jpg", f.read())
+                os.remove(heightmap)
+                os.remove(f"{heightmap}.aux.xml")
         else:
             print(
                 "Failed to communicate with layout service",
