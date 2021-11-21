7aa14bff3a37116563138ad94485d8eb28d7b259
tyler <tyler@cruftbusters.com>
2021-11-21 15:31:27 -0600

    websockets!

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import json
 import os
 import sys
 
+import asyncio
 import requests
-import time
+
+import websockets
 
 from generate import generate
 
@@ -12,26 +15,16 @@ sources = [
     for minGroundSpacing, sourcePath in zip(args[::2], args[1::2])
 ]
 
-
-def main():
-    while True:
-        try:
-            tick("https://layouts.painkillergis.com")
-        except Exception as e:
-            print("Failed to process heightmap", e, file=sys.stderr)
-        time.sleep(2.5)
-
-
-def tick(baseURL):
-    response = requests.get(f"{baseURL}/v1/layouts?excludeLayoutsWithHeightmap=true")
-    if response.status_code != 200:
-        raise Exception(f"Got non-2xx status code: {response.status_code}")
-    for layout in response.json():
-        heightmap = generate(sources, layout)
-        with open(heightmap, 'rb') as f:
-            requests.put(f"{baseURL}/v1/layouts/{layout['id']}/heightmap.jpg", f.read())
-        os.remove(heightmap)
-        os.remove(f"{heightmap}.aux.xml")
+async def main():
+    async with websockets.connect("wss://layouts.painkillergis.com/v1/layouts_awaiting") as ws:
+        while True:
+            layout = json.loads(await ws.recv())
+            heightmap = generate(sources, layout)
+            with open(heightmap, 'rb') as f:
+                requests.put(f"https://layouts.painkillergis.com/v1/layouts/{layout['id']}/heightmap.jpg", f.read())
+            os.remove(heightmap)
+            os.remove(f"{heightmap}.aux.xml")
+            await ws.send("")
 
 
-main()
+asyncio.run(main())
